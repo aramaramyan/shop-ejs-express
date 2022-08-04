e0ea01477b7e4820cc170113ff9e77a913245220
aramaramyan <aramaramyan1997@mail.ru>
2022-08-04 12:53:37 +0400

    feat: add routes

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/add-product", (req, res) => {
-  res.send(`<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>`);
-});
-
-app.post("/product", (req, res) => {
-  console.log(`:::req.body:::`, req.body);
-  res.redirect("/");
-});
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1>`);
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
